d485c46c19db1dd20340be3ccb7677afc77457ce
namprungs <kuyyamasdfg@gmail.com>
2024-03-23 01:15:23 +0700

    edit route and banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -11,16 +11,15 @@ export default function Banner() {
     const router = useRouter();
 
     const { data : session } = useSession();
-
     return (
         <div className={styles.banner} onClick={()=>{setIndex(index+1)}}>
-            <Image src={covers[index%4]} alt='cover' fill={true} objectFit='cover'/>
+            <Image className='object-cover' src={covers[index%4]} alt='cover' priority fill={true}/>
             <div className="relative mt-[80px] z-20 text-center bg-opacity-45 bg-cyan-50 p-4">
                 <h1 className='text-4xl font-medium text-cyan-950'>Dentist Appointment</h1>
                 <h3 className='text-xl font-serif text-cyan-950'>Let's come and take vaccine</h3>
             </div>
             {
-                session? <div className='z-30 absolute top-5 left-5 text-yellow-50 text-xl'> Welcome {session.user?.name} </div> : null
+                session? <div className='z-30 absolute top-5 left-5 text-yellow-50 text-xl'> Welcome {session.user.name} </div> : null
             }
             <button className='bg-white text-cyan-600 border-2 border-cyan-600 font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 right-0 hover:bg-cyan-600 hover:text-white hover:border-transparent' onClick={(e)=>{e.stopPropagation(); router.push('/hospital')}}>
                 Select Dentist
