bd96121a092f43e5ea38e9d9e3772404c8eb89dd
namprungs <kuyyamasdfg@gmail.com>
2024-03-23 02:55:29 +0700

    edit loginpage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -36,20 +36,96 @@ function LoginPage() {
         console.log("Error from login" + error);
     }
   }
+
+
   return (
-    <div className='container mx-auto flex flex-col justify-center items-center h-screen '>
-        <form onSubmit={handlerSubmit} className='flex flex-col items-center w-fit h-fit px-2 py-3 rounded-md bg-gray-500 shadow-md'>
-            <input onChange={(e)=>setEmail(e.target.value)} className='block bg-gray-300 p-2 my-2 rounded-md' type="email" placeholder='Enter your email' />
-            <input onChange={(e)=>setPassword(e.target.value)} className='block bg-gray-300 p-2 my-2 rounded-md' type="password" placeholder='Enter your password'/>
-            <button type="submit" className='my-2 w-full bg-green-500 p-2 rounded-md text-white'>Login</button>
-            {error && (
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handlerSubmit}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                onChange={(e)=>setEmail(e.target.value)}
+                className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                onChange={(e)=>{setPassword(e.target.value); console.log(password);}}
+                // required
+                className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+          {error && (
                 <div className=' text-center bg-red-500 w-fit text-sm text-white py-1 px-3 rounded-md mt-2'>
                     {error}
                 </div>
             )}
         </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Do not have an account?
+          <a
+            href="#"
+            className="ml-2 font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Sign-up
+          </a>
+        </p>
+      </div>
+      
     </div>
   )
 }
 
 export default LoginPage
+
+
+
+/**/
