2b02cfc6332f2e717553abe120a6cae779f57f42
namprungs <kuyyamasdfg@gmail.com>
2024-03-23 01:14:45 +0700

    add trycatch in libs

--- src/libs/userLogIn.tsx
+++ src/libs/userLogIn.tsx
@@ -1,18 +1,22 @@
 export default async function userLogin(userEmail:string, userPassword:string) {
-
-    const response = await fetch("https://privacy-backend.vercel.app/api/v1/auth/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            email: userEmail,
-            password: userPassword
-        }),
-    })
-    if(!response.ok) {
-        throw new Error("Failed to log-in")
+    try {
+        const response = await fetch("https://privacy-backend.vercel.app/api/v1/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: userEmail,
+                password: userPassword
+            }),
+        })
+        if(!response.ok) {
+            throw new Error("Failed to log-in")
+        }
+    
+        return await response.json()
+    } catch (error) {
+        console.log("error in userLogin is ",error);
     }
 
-    return await response.json()
 }
