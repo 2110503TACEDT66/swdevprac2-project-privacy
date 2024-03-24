e4278c393fcd6d9346be99b4efb80ac8b3d92f01
namprungs <kuyyamasdfg@gmail.com>
2024-03-24 23:03:15 +0700

    add register page and libs

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,78 @@
+"use client";
+import LoadingProgress from "@/components/LoadingProgress";
+import userRegister from "@/libs/userRegister";
+import { signIn } from "next-auth/react";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 function page() {
+  const router = useRouter();
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [tel, setTel] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState(false);
+
+  const handlerSubmit = async (e: any) => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+
+      if (!email || !password || !name || !tel) {
+        setError("Please enter complete information.");
+        setLoading(false);
+        return;
+      }
+
+      if (password.length <= 6) {
+        setError("Password must have more than 6 ");
+        setLoading(false);
+        return;
+      }
+
+      const telRegex = /^\d+$/;
+      if (tel.length !== 10) {
+        setError("Please enter a valid phone number with 10 digits.");
+        setLoading(false);
+        return;
+      }
+      if (!telRegex.test(tel)) {
+        setError("Please enter a only digits");
+        setLoading(false);
+        return;
+      }
+
+      const user = await userRegister(name, email, password, tel);
+      console.log({ user });
+      if (!user) {
+        setError("Register Error");
+        setLoading(false);
+        return;
+      }
+
+      const res = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+
+      if (res?.error) {
+        setError("Invalid credentials");
+        setLoading(false);
+        return;
+      }
+
+      console.log("going to router");
+      router.refresh();
+      router.replace("/");
+    } catch (error) {
+      console.log("Error from login" + error);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -16,7 +87,7 @@ function page() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#">
+        <form className="space-y-6" onSubmit={handlerSubmit}>
           <div>
             <label
               htmlFor="name"
@@ -30,6 +101,7 @@ function page() {
                 name="name"
                 type="name"
                 required
+                onChange={(e) => setName(e.target.value)}
                 className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -47,6 +119,7 @@ function page() {
                 name="email"
                 type="email"
                 required
+                onChange={(e) => setEmail(e.target.value)}
                 className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -60,7 +133,6 @@ function page() {
               >
                 Password
               </label>
-              
             </div>
             <div className="mt-2">
               <input
@@ -68,27 +140,29 @@ function page() {
                 name="password"
                 type="password"
                 required
+                onChange={(e) => setPassword(e.target.value)}
                 className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
-            <div>
-              <label
-                htmlFor="tel"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Tel
-              </label>
-              <div className="mt-2">
-                <input
-                  id="tel"
-                  name="tel"
-                  type="tel"
-                  required
-                  className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+          <div>
+            <label
+              htmlFor="tel"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Tel
+            </label>
+            <div className="mt-2">
+              <input
+                id="tel"
+                name="tel"
+                type="tel"
+                required
+                onChange={(e) => setTel(e.target.value)}
+                className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
             </div>
+          </div>
 
           <div>
             <button
@@ -98,6 +172,11 @@ function page() {
               Sign up
             </button>
           </div>
+          {error && (
+            <div className=" text-center bg-red-500 w-fit text-sm text-white py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
         </form>
 
         <p className="mt-10 text-center text-sm text-gray-500">
@@ -110,6 +189,7 @@ function page() {
           </Link>
         </p>
       </div>
+      <LoadingProgress show={loading} />
     </div>
   );
 }
---
+++ src/libs/userRegister.tsx
@@ -0,0 +1,28 @@
+export default async function userRegister(userName:string,userEmail:string, userPassword:string,userTel:string) {
+    try {
+        console.log("entry to register");
+        const response = await fetch("https://privacy-backend.vercel.app/api/v1/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "mode": "cors"
+            },
+            body: JSON.stringify({
+                name: userName,
+                email: userEmail,
+                password: userPassword,
+                tel: userTel
+            }),
+        })
+
+        if(!response.ok) {
+            throw new Error("Failed to Register")
+        }
+    
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.log("error in userLogin is ",error);
+    }
+
+}
