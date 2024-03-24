5a29969825934a08ae17ff341431b8249f4faccb
namprungs <kuyyamasdfg@gmail.com>
2024-03-25 00:26:32 +0700

    change design hospitalCatalog

--- src/components/HospitalCatalog.tsx
+++ src/components/HospitalCatalog.tsx
@@ -1,20 +1,26 @@
 import Link from "next/link";
 import Card from "./Card";
 import { DentistItem, DentistJson } from "../../interface";
+import { useWindowListener } from "@/hooks/useWindowListener";
 
-export default async function HospitalCatalog({hospitalsJson} : {hospitalsJson:Promise<DentistJson>}) {
+export default async function HospitalCatalog({
+  hospitalsJson,
+}: {
+  hospitalsJson: Promise<DentistJson>;
+}) {
+  const Hospital = await hospitalsJson;
 
-    const Hospital = await hospitalsJson;
-
-    return (
-        <div style={{margin:"20px", display:"flex", flexDirection:"row", flexWrap:"wrap", justifyContent:"space-around", alignContent:"space-around"}}>
-            {
-                Hospital.data.map((DentistItem:DentistItem)=>(
-                    <Link key={DentistItem.id} href={`/hospital/${DentistItem.id}`} className="w-1/5">
-                        <Card hospitalName={DentistItem.name} imgSrc={'/img/doctor.jpg'}/>
-                    </Link>
-                ))
-            }
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center w-full gap-x-4 gap-y-6">
+      {Hospital.data.map((DentistItem: DentistItem) => (
+        <Link
+          key={DentistItem.id}
+          href={`/hospital/${DentistItem.id}`}
+          className="w-full"
+        >
+          <Card hospitalName={DentistItem.name} imgSrc={"/img/doctor.jpg"} />
+        </Link>
+      ))}
+    </div>
+  );
 }
