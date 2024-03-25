b07b37456171f60d35526ca9d4aeef132594b710
torrak <156417791+torrak@users.noreply.github.com>
2024-03-26 00:49:27 +0700

    edit dentist

--- src/components/DentistCatalog.tsx
+++ src/components/DentistCatalog.tsx
@@ -1,7 +1,5 @@
-import Link from "next/link";
 import Card from "./Card";
 import { DentistItem, DentistJson } from "../../interface";
-import { useWindowListener } from "@/hooks/useWindowListener";
 
 export default async function DentistCatalog({
   dentistsJson,
@@ -12,8 +10,8 @@ export default async function DentistCatalog({
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center w-full gap-x-4 gap-y-6">
-      {dentist.data.map((dentistItem: DentistItem) => (
-          <Card dentistName={dentistItem.name} dentistDesc = {{dentistExp:dentistItem.years_of_experience,dentistExpertise:dentistItem.area_of_expertise}} imgSrc={"/img/doctor.jpg"} />
+      {dentist.data.map((DentistItem: DentistItem) => (
+          <Card dentistName={DentistItem.name} dentistDesc = {{dentistExp:DentistItem.years_of_experience,dentistExpertise:DentistItem.area_of_expertise}} imgSrc={"/img/doctor.jpg"} />
       ))}
     </div>
   );
