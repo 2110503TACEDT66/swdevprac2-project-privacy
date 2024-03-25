b52544b9b74e584e15917670b2b6eced281d5f57
namprungs <kuyyamasdfg@gmail.com>
2024-03-25 16:20:39 +0700

    apptlist

--- src/components/AppointmentList.tsx
+++ src/components/AppointmentList.tsx
@@ -7,11 +7,18 @@ import { Appointment, Appointments, DentistItem } from "../../interface";
 import getAppointments from "@/libs/getAppointments";
 import { useSession } from "next-auth/react";
 import dayjs from "dayjs";
+import deleteAppointment from "@/libs/deleteAppointment";
 
 export default function AppointmentList() {
+
     const [bookingItems , setBookingItems] = useState<Appointment[]>([]);
     const dispatch = useDispatch<AppDispatch>()
     const {data:session} = useSession()
+
+    const deletemyAppointment = async(ApptId:string)=>{
+      if(session)
+        await deleteAppointment(ApptId,session?.user.token)
+    }
     useEffect(() => {
         if(session){
             const fetchAppointments = async () => {
@@ -31,15 +38,18 @@ export default function AppointmentList() {
     return (
         <>
         {
-            bookingItems.length>0?
+            bookingItems?
             bookingItems.map((item:Appointment) => (
                 <div className="bg-slate-200 rounded px-5 mx-5 py-2 my-2" key={item._id}>
                     <div className="text-xl m-1"></div>
                     <div className="text-sm m-1">Id: {item._id}</div>
                     <div className="text-sm m-1">Dentist: {(item.dentist).name}</div>
                     <div className="text-sm m-1">Date: {dayjs(item.apptDate).format("YYYY/MM/DD")}</div>
-                    <button className='block h-[10%] text-sm rounded-md bg-cyan-800 hover:bg-slate-700 my-2 p-2 text-white shadow-sm' onClick = {() => dispatch(removeBooking(item._id))}>
-                        Remove this appointment
+                    <button className='h-[10%] text-sm rounded-md bg-cyan-800 hover:bg-slate-700 m-2 p-2 text-white shadow-sm' onClick = {() => dispatch(removeBooking(item._id))}>
+                        Remove
+                    </button>
+                    <button className='h-[10%] text-sm rounded-md bg-cyan-800 hover:bg-slate-700 m-2 p-2 text-white shadow-sm' onClick = {() => dispatch(removeBooking(item._id))}>
+                        Edit
                     </button>
                 </div>
             )) : <div className="m-4 text-cyan-800">No Appointment</div>
