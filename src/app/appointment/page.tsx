e1d90cc21485f8250874ad4fb322a9d7f08d604c
torrak <156417791+torrak@users.noreply.github.com>
2024-03-25 17:14:16 +0700

    edit appointment

--- src/app/appointment/page.tsx
+++ src/app/appointment/page.tsx
@@ -18,19 +18,11 @@ import LoadingProgress from "@/components/LoadingProgress";
 export default function Booking() {
     const { data : session } = useSession();
 
-    //For show login user
-    // const session = await getServerSession(authOptions)
-    // if(!session || !session.user.token) return null;
-    // const profile = await getUserProfile(session.user.token);
-    // var createdAt = new Date(profile.data.createdAt);
-
     const dispatch = useDispatch<AppDispatch>();
-    const [bookingName, setBookingName] = useState<string>("");
-    const [bookingSurname, setBookingSurname] = useState<string>("");
-    const [bookingId, setBookingId] = useState<string>("");
     const [bookingDentist, setBookingDentist] = useState<string>("");
     const [bookingDate, setBookingDate] = useState<Dayjs | null>(null);
     const [loading, setLoading] = useState(false);
+    
     const makeAppointment = async () => {
         setLoading(true);
         if(session){
@@ -46,22 +38,8 @@ export default function Booking() {
 
     return (
         <main className="flex flex-col items-center">
-
-            {/* show login user
-            <table className="table-auto border-separate border-spacing-2 mt-3 bg-slate-100 p-3 rounded">
-                <tbody>
-                    <tr><td>Name</td><td>{profile.data.name}</td></tr>
-                    <tr><td>Email</td><td>{profile.data.email}</td></tr>
-                    <tr><td>Tel.</td><td>{profile.data.tel}</td></tr>
-                    <tr><td>Member Since </td><td>{createdAt.toString()}</td></tr>
-                </tbody>
-            </table> */}
-
             <h1 className="text-[20px] m-5 font-bold text-cyan-700 rounded-lg p-3">Make an Appointment</h1>
             <form className="w-[40vw] h-auto bg-violet-50 justify-center rounded-lg flex flex-col p-10">
-                {/* <TextField name="Name" label="Name" variant="standard" value={bookingName} className="m-3" onChange={(e) => setBookingName(e.target.value)}/>
-                <TextField name="Lastname" label="Lastname" variant="standard" value={bookingSurname} className="m-3" onChange={(e) => setBookingSurname(e.target.value)}/>
-                <TextField name="Citizen ID" label="Citizen ID" variant="standard" value={bookingId} className="m-3" onChange={(e) => setBookingId(e.target.value)}/> */}
                 <DentistDateReserve onDateChange={(value:Dayjs) => {setBookingDate(value)}} onDentistChange={(value:string) => {setBookingDentist(value)}}/>
                 <button name='Book Vaccine' type="button" 
                 className="font-semibold bg-cyan-700 text-yellow-100 rounded-md m-3 p-3" onClick={makeAppointment}>Book Vaccine</button>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -186,7 +186,7 @@ function page() {
         </form>
 
         <p className="mt-10 text-center text-sm text-gray-500">
-          Already have an account?
+          Already have an account? 
           <Link
             href="/login"
             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
