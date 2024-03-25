38bda52fae13ff01db93e339605ad1183ec5de8a
namprungs <kuyyamasdfg@gmail.com>
2024-03-26 00:53:18 +0700

    ngongmak

--- src/app/dentist/page.tsx
+++ src/app/dentist/page.tsx
@@ -6,7 +6,7 @@ import { Suspense } from "react";
 export default function hospital(){
     const dentists = getDentists();
     return(
-        <main className="p-5 ml-16 flex flex-col items-center justify-center space-y-3">
+        <main className="p-5 ml-6 flex flex-col items-center justify-center space-y-3">
             <h1 className="text-center text-3xl font-semibold mt-2 mb-5 text-[#107557]">Select Your Dentist</h1>
             <Suspense fallback={<p>Loading ... <LinearProgress/></p>}>
                 <DentistCatalog dentistsJson={dentists}/>
