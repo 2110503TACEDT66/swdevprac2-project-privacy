167983da013cb978085e3e5d6dded23dc68212c4
torrak <156417791+torrak@users.noreply.github.com>
2024-03-25 22:50:22 +0700

    edit dentist card

--- src/app/(hospitalinfo)/hospital/page.tsx
+++ src/app/(hospitalinfo)/hospital/page.tsx
@@ -8,7 +8,7 @@ export default function hospital(){
     const hospitals = getDentists();
     return(
         <main className="p-5 ml-16 flex flex-col items-center justify-center space-y-3">
-            <h1 className="text-center text-3xl font-semibold mt-2 mb-5 text-[#107557]">Select Your Hospital</h1>
+            <h1 className="text-center text-3xl font-semibold mt-2 mb-5 text-[#107557]">Select Your Dentist</h1>
             <Suspense fallback={<p>Loading ... <LinearProgress/></p>}>
                 <HospitalCatalog hospitalsJson={hospitals}/>
             </Suspense>
