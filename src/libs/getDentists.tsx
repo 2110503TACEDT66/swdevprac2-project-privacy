10f2433fd7f0bc09dd92539f4f196f84699d9e25
torrak <156417791+torrak@users.noreply.github.com>
2024-03-26 22:03:21 +0700

    edit .env.local

--- src/libs/getDentists.tsx
+++ src/libs/getDentists.tsx
@@ -1,5 +1,5 @@
 export default async function getDentists() {
-    const response = await fetch(`${process.env.BACKEND_URL}'/api/v1/dentists`)
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/dentists`)
     if(!response.ok) {
         throw new Error("Failed to fetch dentists")
     }
