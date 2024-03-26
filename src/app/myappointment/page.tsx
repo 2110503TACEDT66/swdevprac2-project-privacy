fd393ad5442a3c7acd7ea112cab957dc59ab8315
torrak <156417791+torrak@users.noreply.github.com>
2024-03-26 22:28:40 +0700

    edit myappointment page

--- src/app/myappointment/page.tsx
+++ src/app/myappointment/page.tsx
@@ -1,9 +1,20 @@
 'use client'
 import AppointmentList from "@/components/AppointmentList"
+import { useSession } from "next-auth/react";
 
 export default function MyAppointment() {
+    const { data: session } = useSession();
+    if (session?.user.role==='admin') {
+        return (
+            <div className="h-full overflow-y-auto bg-[url('/img/apptcover.jpg')] bg-cover flex items-center justify-center ">
+            <main>
+                <AppointmentList></AppointmentList>
+            </main>
+            </div>
+        )
+    }
     return (
-        <div className="h-screen overflow-y-auto bg-[url('/img/apptcover.jpg')] bg-cover flex items-center justify-center ">
+        <div className="h-screen bg-[url('/img/apptcover.jpg')] bg-cover flex items-center justify-center ">
         <main>
             <AppointmentList></AppointmentList>
         </main>
