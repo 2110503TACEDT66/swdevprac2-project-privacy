21e98cde0e52531d6bc713efc654721c697487fd
namprungs <kuyyamasdfg@gmail.com>
2024-03-26 22:36:04 +0700

    edit

--- src/components/AppointmentList.tsx
+++ src/components/AppointmentList.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { Appointment, Appointments, DentistItem } from "../../interface";
@@ -19,50 +19,86 @@ export default function AppointmentList() {
   const [loadingData, setLoadingData] = useState(true);
 
   const deletemyAppointment = async (ApptId: string) => {
-      setLoading(true);
-      try {
-          if (session) {
-              const res = await deleteAppointment(ApptId, session?.user.token);
-              console.log(res);
-              setLoading(false);
-              setRefresh(!refresh);
-              
-          }
-      } catch (error) {
-          console.log(error);
-          setLoading(false);
+    setLoading(true);
+    try {
+      if (session) {
+        const res = await deleteAppointment(ApptId, session?.user.token);
+        console.log(res);
+        setLoading(false);
+        setRefresh(!refresh);
       }
-
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
-      if (session) {
-          const fetchAppointments = async () => {
-              setLoadingData(true);
-              try {
-                  const data = await getAppointments(session.user.token);
-                  if (data) setCheck(true);
-                  setBookingItems(data.data);
-                  setLoadingData(false);
-                  console.log(data.data);
-              } catch (error) {
-                  console.error("Failed to fetch dentists:", error);
-                  setLoadingData(false); 
-              }
-          };
+    if (session) {
+      const fetchAppointments = async () => {
+        setLoadingData(true);
+        try {
+          const data = await getAppointments(session.user.token);
+          if (data) setCheck(true);
+          setBookingItems(data.data);
+          setLoadingData(false);
+          console.log(data.data);
+        } catch (error) {
+          console.error("Failed to fetch dentists:", error);
+          setLoadingData(false);
+        }
+      };
 
-          fetchAppointments();
-      }
+      fetchAppointments();
+    }
   }, [session, refresh]);
 
-    return (
-        <div className="w-[60vw] justify-center text-center rounded-lg flex flex-col p-10">
-            <p className="text-3xl font-bold text-neutral-50 py-8">My Appointment</p>
-            {loadingData && <LoadingProgress show={true} />}
-            {!loadingData && bookingItems.length === 0 && <div className="text-amber-50 text-l">No Appointments</div>}
-            {bookingItems ? (
-                bookingItems.map((item: Appointment) => (
-                    <div
+  return (
+    <div className="w-[60vw] justify-center text-center rounded-lg flex flex-col p-10">
+      <p className="text-3xl font-bold text-neutral-50 py-8">My Appointment</p>
+      {loadingData && <LoadingProgress show={true} />}
+      {!loadingData && bookingItems.length === 0 && (
+        <div className="text-amber-50 text-l">No Appointments</div>
+      )}
+      {bookingItems
+        ? bookingItems.map((item: Appointment) => (
+            <div className="flex justify-between self-center bg-lime-50 w-[500px] h-[150px] rounded-[17px] px-5 py-4 my-2">
+              <div className="w-40 flex flex-col text-left mr-4 text-teal-900">
+                <div className="font-semibold text-sm my-1">
+                  Name: {item.user.name}
+                </div>
+                <div className="font-semibold text-sm my-1">
+                  Dentist: {item.dentist.name}
+                </div>
+                <div className="font-semibold text-sm my-1">
+                  Date: {dayjs(item.apptDate).format("YYYY/MM/DD")}
+                </div>
+              </div>
+              <div className="w-60 flex flex-row justify-between items-center">
+                <div className="flex flex-row space-x-5">
+                  <button
+                    className="w-[105px] h-[45px]text-sm rounded-md bg-rose-900 hover:bg-slate-700 m-2 p-2 text-white shadow-sm"
+                    onClick={() => deletemyAppointment(item._id)}
+                  >
+                    Remove
+                  </button>
+                  <button
+                    className="w-[105px] h-[45px]text-sm rounded-md bg-teal-900 hover:bg-slate-700 m-2 p-2 text-white shadow-sm"
+                    onClick={() => router.replace(`/appointment/${item._id}`)}
+                  >
+                    Edit
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))
+        : null}
+      <LoadingProgress show={loading} />
+    </div>
+  );
+}
+
+/*<div
                         className="bg-lime-50 w-[500px] h-[150px] rounded-[17px] px-5 py-4 my-2 flex flex-row sm:flex-row justify-center items-center self-center"
                         key={item._id}
                     >
@@ -85,12 +121,4 @@ export default function AppointmentList() {
                                 Edit
                             </button>
                         </div>
-                    </div>
-                ))
-            ) : (
-                null
-            )}
-            <LoadingProgress show={loading} />
-        </div>
-    );
-}
+                    </div>*/
